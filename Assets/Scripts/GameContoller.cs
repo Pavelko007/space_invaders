2fb5fb61201134f3e17c85fbb8c0c5386aa6d98b
Pavlo Kvas <kvaspasha@gmail.com>
2018-04-17 08:40:45 +0300

    no message

---
+++ Assets/Scripts/GameContoller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameContoller : MonoBehaviour
+{
+    public GameObject playerPrefab;
+
+    public Transform playerSpawnPos;
+
+	void Start ()
+	{
+	    Instantiate(playerPrefab, playerSpawnPos.position, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
