c360692eb9f00445b061314ee98179a5a47c3c7e
Pavlo Kvas <kvaspasha@gmail.com>
2018-04-17 16:24:15 +0300

    no message

---
+++ Assets/Scripts/FloatingScoreController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingScoreController : MonoBehaviour
+{
+    public Text scoreText;
+
+    void Awake()
+    {
+        Invoke("Destroy", 1.5f);
+    }
+
+    public void SetScore(int score)
+    {
+        scoreText.text = string.Format("+{0}", score);
+    }
+
+    void Destroy()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/ScoreUpdater.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreUpdater : MonoBehaviour
+{
+    public float scoreForKill;
+    public GameObject floatingScore;
+
+    // Update is called once per frame
+    public void UpdateScore ()
+	{
+	    GameContoller.Instance.AddScore(scoreForKill);
+	    GameObject gameObject = Instantiate(floatingScore, transform.position, Quaternion.identity);
+        gameObject.GetComponent<FloatingScoreController>().SetScore((int) scoreForKill);
+	    gameObject.transform.position = transform.position;
+	}
+}
