f066e480bbcf1675a9cab56144a11a22630bb8e1
Pavlo Kvas <kvaspasha@gmail.com>
2018-04-17 08:35:55 +0300

    no message

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    var hor = Input.GetAxis("Horizontal");
+	    if (hor != 0)
+	    {
+	        Vector3 dir = Vector3.zero;
+	        if (hor > 0)
+	        {
+	            dir = Vector3.right;
+	        }
+            else if (hor < 0)
+	        {
+	            dir = Vector3.left;
+	        }
+
+            transform.Translate(dir*10*Time.deltaTime);
+	    }
+	}
+}
