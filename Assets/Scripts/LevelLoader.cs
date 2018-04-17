2cf9f4c99691e00d8a6059814b88bf9346db49d1
Pavlo Kvas <kvaspasha@gmail.com>
2018-04-17 08:25:48 +0300

    no message

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+	// Update is called once per frame
+    public void LoadLevel() {
+		SceneManager.LoadScene("main");
+	}
+}
