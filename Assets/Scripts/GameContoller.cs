e165ccca25005adb506d9173f059d31f7a3dff69
Pavlo Kvas <kvaspasha@gmail.com>
2018-04-17 09:01:51 +0300

    no message

--- Assets/Scripts/GameContoller.cs
+++ Assets/Scripts/GameContoller.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameContoller : MonoBehaviour
 {
@@ -8,11 +9,28 @@ public class GameContoller : MonoBehaviour
 
     public Transform playerSpawnPos;
 
+    public Text timerText;
+
 	void Start ()
 	{
 	    Instantiate(playerPrefab, playerSpawnPos.position, Quaternion.identity);
+
+	    StartCoroutine(Countdown());
 	}
-	
+
+    IEnumerator Countdown()
+    {
+        float timeToStart = 3;
+        while (timeToStart > 0)
+        {
+            timeToStart -= Time.deltaTime;
+            timerText.text = ((int)Mathf.Ceil(timeToStart)).ToString();
+            yield return new WaitForEndOfFrame();
+        }
+        timerText.gameObject.SetActive(false);
+        yield return null;
+    }
+
 	// Update is called once per frame
 	void Update () {
 		
